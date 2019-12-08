47e04fc797035f270a150ad7df5663d338700133
erlandnafidmk <erland.nafid@mobilkamu.com>
2019-12-09 02:35:23 +0700

    Updates

---
+++ precache-manifest.3fe011893f880c96486163c90cd9aeed.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "976e216b1ab3495192bcd3d309e3f566",
+    "url": "/test-sindata/index.html"
+  },
+  {
+    "revision": "78df02e158b693fcd4ae",
+    "url": "/test-sindata/static/css/2.7f669fb7.chunk.css"
+  },
+  {
+    "revision": "82f7f8dce8980aff76b5",
+    "url": "/test-sindata/static/css/main.ad09a65a.chunk.css"
+  },
+  {
+    "revision": "78df02e158b693fcd4ae",
+    "url": "/test-sindata/static/js/2.62d3123b.chunk.js"
+  },
+  {
+    "revision": "82f7f8dce8980aff76b5",
+    "url": "/test-sindata/static/js/main.fdcda2e4.chunk.js"
+  },
+  {
+    "revision": "85aed1dd603fdb06d340",
+    "url": "/test-sindata/static/js/runtime-main.22da05c5.js"
+  }
+]);
